19e9b624bf199f4b9651922e9e58f8be1928a9ea
fugasjunior <mail@martinfunda.cz>
2020-05-16 10:26:15 +0200

    disable security while dev profile is active

--- src/main/java/cz/forgottenempire/arma3servergui/config/WebSecurityConfig.java
+++ src/main/java/cz/forgottenempire/arma3servergui/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile("!dev")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
---
+++ src/main/java/cz/forgottenempire/arma3servergui/config/dev/DevWebConfig.java
@@ -0,0 +1,19 @@
+package cz.forgottenempire.arma3servergui.config.dev;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+@EnableWebMvc
+public class DevWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("POST", "GET", "DELETE");
+    }
+}
+
