f2496d606eae50749f6c5d5215b126ec1348e568
fugasjunior <mail@martinfunda.cz>
2020-10-18 16:44:38 +0200

    encrypt steam password in database

--- src/main/java/cz/forgottenempire/arma3servergui/model/SteamAuth.java
+++ src/main/java/cz/forgottenempire/arma3servergui/model/SteamAuth.java
@@ -1,10 +1,12 @@
 package cz.forgottenempire.arma3servergui.model;
 
+import com.bol.secure.Encrypted;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @AllArgsConstructor
@@ -15,7 +17,8 @@ public class SteamAuth {
     @Id
     private Long id;
     private String username;
-    // TODO add encrypting
+    @Field
+    @Encrypted
     private String password;
     private String steamGuardToken;
 }
