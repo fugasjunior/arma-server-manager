a838df7c4ab91612f40a47ce3fe3306152971a53
fugasjunior <mail@martinfunda.cz>
2021-01-17 10:55:30 +0100

    cors hotfix

--- src/main/java/cz/forgottenempire/arma3servergui/config/WebSecurityConfig.java
+++ src/main/java/cz/forgottenempire/arma3servergui/config/WebSecurityConfig.java
@@ -45,8 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and()
+        http.cors().disable()
                 .authorizeRequests()
                 .antMatchers("/static/**", "/{spring:[^(api)]}**", "/").permitAll()
                 .antMatchers("/login").permitAll()
