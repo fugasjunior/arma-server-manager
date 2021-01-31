c5a76e677173c63a8186634bb3fd17ccf1964eb9
fugasjunior <mail@martinfunda.cz>
2021-01-31 21:57:17 +0100

    fix authentication bugs

--- src/main/webapp/src/services/scenarioService.js
+++ src/main/webapp/src/services/scenarioService.js
@@ -8,19 +8,14 @@ export function getScenarios() {
 }
 
 export function downloadScenario(name) {
-    // return window.open(apiEndpoint + "/" + name);
-    // return http.post()
     http.get(apiEndpoint + "/" + name, {responseType: 'blob'})
     .then(response => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
         link.setAttribute('download', name);
-        // 3. Append to html page
         document.body.appendChild(link);
-        // 4. Force download
         link.click();
-        // 5. Clean up and remove the link
         link.parentNode.removeChild(link);
     })
 }
