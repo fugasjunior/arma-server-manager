3f2665d75f903a6749df657c735863c3f3a026e4
fugasjunior <mail@martinfunda.cz>
2022-10-29 23:10:05 +0200

    improve mods list builder style, add more mods to test data

--- src/main/webapp/src/UI/ListBuilder.js
+++ src/main/webapp/src/UI/ListBuilder.js
@@ -1,6 +1,17 @@
 import React, {useState} from "react";
 import styles from "./ListBuilder.module.css";
-import {Box, FormControl, Grid, InputLabel, List, ListItemButton, Select, TextField, Typography} from "@mui/material";
+import {
+    Box,
+    FormControl,
+    Grid,
+    InputLabel,
+    List,
+    ListItemButton,
+    Select,
+    Stack,
+    TextField,
+    Typography
+} from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 
 const ListBuilder = props => {
@@ -18,7 +29,7 @@ const ListBuilder = props => {
     const presetAvailable = !!props.presets && props.presets.length > 0;
 
     return (
-            <Box className={styles.box} sx={{
+            <Box className={styles.box} overflow="auto" sx={{
                 bgcolor: 'background.paper',
                 boxShadow: 24,
                 p: 4,
@@ -53,27 +64,38 @@ const ListBuilder = props => {
                     }
 
                     <Grid item xs={6}>
-                        <Typography variant="h5">Available {itemsLabel}</Typography>
-                        <TextField label="Filter" type="search" variant="standard" onChange={handleFilterChange}/>
-                        <List className={styles.list}>
-                            {filteredAvailableOptions.length === 0 && <span>No {itemsLabel} available</span>}
-                            {filteredAvailableOptions.length > 0 && filteredAvailableOptions.map(opt =>
-                                    <ListItemButton key={opt.id} onClick={() => props.onSelect(opt)}>
-                                        {opt.name}
-                                    </ListItemButton>
-                            )}
-                        </List>
+                        <Stack>
+                            <Stack direction="row" spacing={4} mb={1}>
+                                <Typography variant="h5">Available {itemsLabel}</Typography>
+                                <TextField label="Filter" type="search" variant="outlined" size="small" margin="none"
+                                           onChange={handleFilterChange}/>
+                            </Stack>
+                            <List className={styles.list}>
+                                {filteredAvailableOptions.length === 0 && <Typography
+                                        m={2}>No {itemsLabel} available</Typography>}
+                                {filteredAvailableOptions.length > 0 && filteredAvailableOptions.map(opt =>
+                                        <ListItemButton key={opt.id} onClick={() => props.onSelect(opt)} divider>
+                                            {opt.name}
+                                        </ListItemButton>
+                                )}
+                            </List>
+                        </Stack>
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography variant="h5">Enabled {itemsLabel}</Typography>
-                        <List className={styles.list}>
-                            {props.selectedOptions.length === 0 && <span>No {itemsLabel} selected</span>}
-                            {props.selectedOptions.length > 0 && props.selectedOptions.map(opt =>
-                                    <ListItemButton key={opt.id} onClick={() => props.onDeselect(opt)}>
-                                        {opt.name}
-                                    </ListItemButton>
-                            )}
-                        </List>
+                        <Stack>
+                            <Stack direction="row" spacing={4} mb={2} alignItems="flex-end">
+                                <Typography variant="h5">Enabled {itemsLabel}</Typography>
+                            </Stack>
+                            <List className={styles.list}>
+                                {props.selectedOptions.length === 0 && <Typography
+                                        m={2}>No {itemsLabel} selected</Typography>}
+                                {props.selectedOptions.length > 0 && props.selectedOptions.map(opt =>
+                                        <ListItemButton key={opt.id} onClick={() => props.onDeselect(opt)} divider>
+                                            {opt.name}
+                                        </ListItemButton>
+                                )}
+                            </List>
+                        </Stack>
                     </Grid>
                 </Grid>
             </Box>
