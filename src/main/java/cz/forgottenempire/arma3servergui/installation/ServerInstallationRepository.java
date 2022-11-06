55d757ec0a5ffb9d43d51ce1eba0163cfcf98b01
fugasjunior <mail@martinfunda.cz>
2022-11-06 08:10:29 +0100

    add basic caching for Server Installations and Workshop Mods responses

--- src/main/java/cz/forgottenempire/arma3servergui/installation/ServerInstallationRepository.java
+++ src/main/java/cz/forgottenempire/arma3servergui/installation/ServerInstallationRepository.java
@@ -1,10 +1,16 @@
 package cz.forgottenempire.arma3servergui.installation;
 
 import cz.forgottenempire.arma3servergui.common.ServerType;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface ServerInstallationRepository extends JpaRepository<ServerInstallation, ServerType> {
 
+    @Override
+    @CacheEvict(value = "serverInstallationsResponse", allEntries = true)
+    @NonNull
+    <S extends ServerInstallation> S save(@NonNull S entity);
 }
