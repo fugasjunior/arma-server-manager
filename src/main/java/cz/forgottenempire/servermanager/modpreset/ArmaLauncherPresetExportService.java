01ec8c098d584ac4c2550926874efcf8ed158a53
fugasjunior <mail@martinfunda.cz>
2023-02-24 19:12:35 +0100

    Implement exportModPresetToFile method

--- src/main/java/cz/forgottenempire/servermanager/modpreset/ArmaLauncherPresetExportService.java
+++ src/main/java/cz/forgottenempire/servermanager/modpreset/ArmaLauncherPresetExportService.java
@@ -1,7 +1,37 @@
 package cz.forgottenempire.servermanager.modpreset;
 
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 @Service
 public class ArmaLauncherPresetExportService {
+
+    public static final String TEMPLATE_FILE_NAME = "exportedModPresetHtml.ftl";
+    private final FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @Autowired
+    public ArmaLauncherPresetExportService(FreeMarkerConfigurer freeMarkerConfigurer) {
+        this.freeMarkerConfigurer = freeMarkerConfigurer;
+    }
+
+    public byte[] exportModPresetToFile(ModPreset modPreset) {
+        try {
+            Template template = freeMarkerConfigurer.getConfiguration().getTemplate(TEMPLATE_FILE_NAME);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Writer writer = new OutputStreamWriter(outputStream);
+            template.process(modPreset, writer);
+            writer.flush();
+            return outputStream.toByteArray();
+        } catch (TemplateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/cz/forgottenempire/servermanager/modpreset/ArmaLauncherPresetExportServiceTest.java
+++ src/test/java/cz/forgottenempire/servermanager/modpreset/ArmaLauncherPresetExportServiceTest.java
@@ -1,10 +1,40 @@
 package cz.forgottenempire.servermanager.modpreset;
 
+import cz.forgottenempire.servermanager.common.ServerType;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class ArmaLauncherPresetExportServiceTest {
+
+    public static final String TEMPLATE_FILE_NAME = "exportedModPresetHtml.ftl";
+
     @Test
-    void name() {
-        new ArmaLauncherPresetExportService();
+    void whenExportModPresetToFile_thenCorrectByteArrayReturned() throws IOException, TemplateException {
+        FreeMarkerConfigurer freeMarkerConfigurer = mock(FreeMarkerConfigurer.class);
+        Configuration configuration = mock(Configuration.class);
+        Template template = mock(Template.class);
+        when(freeMarkerConfigurer.getConfiguration()).thenReturn(configuration);
+        when(configuration.getTemplate(TEMPLATE_FILE_NAME)).thenReturn(template);
+        ArmaLauncherPresetExportService presetExportService = new ArmaLauncherPresetExportService(freeMarkerConfigurer);
+        ModPreset preset = new ModPreset("Mod Preset", Collections.emptyList(), ServerType.ARMA3);
+
+        byte[] exportedPreset = presetExportService.exportModPresetToFile(preset);
+
+        assertThat(exportedPreset).isNotNull();
+        InOrder inOrder = inOrder(freeMarkerConfigurer, configuration, template);
+        inOrder.verify(freeMarkerConfigurer).getConfiguration();
+        inOrder.verify(configuration).getTemplate(TEMPLATE_FILE_NAME);
+        inOrder.verify(template).process(eq(preset), any());
+        verifyNoMoreInteractions(freeMarkerConfigurer, configuration, template);
     }
 }
