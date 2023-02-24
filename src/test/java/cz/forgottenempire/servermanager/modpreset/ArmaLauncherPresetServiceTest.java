d0c361a58f0aa194082a9ce4dc9553f726b39a4a
fugasjunior <mail@martinfunda.cz>
2023-02-24 14:33:00 +0100

    Create ArmaLauncherPresetService

---
+++ src/test/java/cz/forgottenempire/servermanager/modpreset/ArmaLauncherPresetServiceTest.java
@@ -0,0 +1,10 @@
+package cz.forgottenempire.servermanager.modpreset;
+
+import org.junit.jupiter.api.Test;
+
+public class ArmaLauncherPresetServiceTest {
+    @Test
+    void name() {
+        new ArmaLauncherPresetService();
+    }
+}
