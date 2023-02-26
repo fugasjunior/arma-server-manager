82eb201c93638627ccf32932fa54c2ee3edd8f70
fugasjunior <mail@martinfunda.cz>
2023-02-26 18:42:50 +0100

    Implement controller methods for log access

--- src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerController.java
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerController.java
@@ -5,35 +5,40 @@ import cz.forgottenempire.servermanager.serverinstance.dtos.ServerDto;
 import cz.forgottenempire.servermanager.serverinstance.dtos.ServerInstanceInfoDto;
 import cz.forgottenempire.servermanager.serverinstance.dtos.ServersDto;
 import cz.forgottenempire.servermanager.serverinstance.entities.Server;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/server")
 @Slf4j
 class ServerController {
 
+    public static final int DEFAULT_LOG_LINES_COUNT = 100;
     private final ServerInstanceService serverInstanceService;
     private final ServerProcessService serverProcessService;
+    private final ServerLogsService logsService;
     private final ServerMapper serverMapper = Mappers.getMapper(ServerMapper.class);
 
     @Autowired
-    public ServerController(ServerInstanceService serverInstanceService, ServerProcessService serverProcessService) {
+    public ServerController(
+            ServerInstanceService serverInstanceService,
+            ServerProcessService serverProcessService,
+            ServerLogsService logsService
+    ) {
         this.serverInstanceService = serverInstanceService;
         this.serverProcessService = serverProcessService;
+        this.logsService = logsService;
     }
 
     @GetMapping
@@ -52,9 +57,7 @@ class ServerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ServerDto> getServer(@PathVariable Long id) {
-        Server server = serverInstanceService.getServer(id)
-                .orElseThrow(
-                        () -> new NotFoundException("Server ID " + id + " doesn't exist"));
+        Server server = getServerEntity(id);
         ServerDto serverDto = serverMapper.mapServerToDto(server);
         ServerInstanceInfo instanceInfo = serverProcessService.getServerInstanceInfo(id);
         serverDto.setInstanceInfo(serverMapper.mapServerInstanceInfoToDto(instanceInfo));
@@ -71,9 +74,7 @@ class ServerController {
 
     @PutMapping("/{id}")
     public ResponseEntity<ServerDto> updateServer(@PathVariable Long id, @Valid @RequestBody ServerDto serverDto) {
-        Server server = serverInstanceService.getServer(id)
-                .orElseThrow(
-                        () -> new NotFoundException("Server ID " + id + " doesn't exist"));
+        Server server = getServerEntity(id);
         serverDto.setId(server.getId());
         serverMapper.updateServerFromDto(serverDto, server);
         server = serverInstanceService.updateServer(server);
@@ -105,5 +106,38 @@ class ServerController {
         log.info("Received request to restart server ID {}", id);
         serverProcessService.restartServer(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/log/download")
+    public ResponseEntity<Resource> downloadLogFile(@PathVariable long id) throws IOException {
+        Server server = getServerEntity(id);
+
+        Resource resource = logsService.getLogFileAsResource(server)
+                .orElseThrow(() -> new NotFoundException("Log file for server '" + server.getName() + "' doesn't exist"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename());
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(resource);
+    }
+
+    @GetMapping("/{id}/log")
+    public ResponseEntity<String> getLastFilesFromLog(@PathVariable long id, @RequestParam(required = false) Integer count) {
+        if (count == null) {
+            count = DEFAULT_LOG_LINES_COUNT;
+        }
+
+        Server server = getServerEntity(id);
+        String logLines = logsService.getLastLinesFromServerLog(server, count);
+        return ResponseEntity.ok(logLines);
+    }
+
+    private Server getServerEntity(long id) {
+        return serverInstanceService.getServer(id)
+                .orElseThrow(
+                        () -> new NotFoundException("Server ID " + id + " doesn't exist"));
     }
 }
