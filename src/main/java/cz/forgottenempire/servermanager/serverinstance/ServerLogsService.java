1e942bc1ecd6fd2cf498dca86daff9d93781fc0a
fugasjunior <mail@martinfunda.cz>
2023-02-26 14:35:55 +0100

    Create ServerLogsService

---
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsService.java
@@ -0,0 +1,7 @@
+package cz.forgottenempire.servermanager.serverinstance;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServerLogsService {
+}
---
+++ src/test/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsServiceTest.java
@@ -0,0 +1,10 @@
+package cz.forgottenempire.servermanager.serverinstance;
+
+import org.junit.jupiter.api.Test;
+
+class ServerLogsServiceTest {
+    @Test
+    void name() {
+        new ServerLogsService();
+    }
+}
