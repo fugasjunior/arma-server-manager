f2e51d41680c0a4f370c7e8f1f0a0a75d3be3492
fugasjunior <mail@martinfunda.cz>
2023-02-26 16:10:26 +0100

    Implement getLastLinesFromServerLog method

--- src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsService.java
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsService.java
@@ -1,7 +1,75 @@
 package cz.forgottenempire.servermanager.serverinstance;
 
+import cz.forgottenempire.servermanager.common.PathsFactory;
+import cz.forgottenempire.servermanager.serverinstance.entities.Server;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 @Service
 public class ServerLogsService {
+
+    private final PathsFactory pathsFactory;
+
+    public ServerLogsService(PathsFactory pathsFactory) {
+        this.pathsFactory = pathsFactory;
+    }
+
+    public String getLastLinesFromServerLog(Server server, int count) {
+        File serverLogFile = pathsFactory.getServerLogFile(server.getType(), server.getId());
+        if (!serverLogFile.exists()) {
+            return "";
+        }
+
+        try {
+            return getLastNLines(serverLogFile, count);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Generated using ChatGTP
+     * This method is efficient for large files such as server logs, as it doesn't load the whole content
+     * into memory
+     */
+    private String getLastNLines(File file, int n) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "r");
+        long fileLength = raf.length();
+        long filePointer = fileLength;
+        int lines = 0;
+        StringBuilder result = new StringBuilder();
+
+        while (filePointer >= 0 && lines < n) {
+            raf.seek(filePointer);
+            if (filePointer == 0) {
+                // If we're at the beginning of the file, read the last line and exit
+                result.insert(0, raf.readLine());
+                break;
+            } else {
+                // Otherwise, read the current character and move back one byte
+                int currentByte = raf.read();
+                filePointer--;
+                if (currentByte == '\n') {
+                    // If we've found a line ending, read the next line
+                    result.insert(0, raf.readLine() + '\n');
+                    lines++;
+                } else if (currentByte == '\r') {
+                    // If we've found a carriage return, read the next byte and check for a line ending
+                    filePointer--;
+                    raf.seek(filePointer);
+                    if (raf.read() == '\n') {
+                        // If the next byte is a line feed, read the next line
+                        result.insert(0, raf.readLine() + '\n');
+                        lines++;
+                    }
+                }
+            }
+        }
+
+        raf.close();
+        return result.toString();
+    }
 }
--- src/test/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsServiceTest.java
+++ src/test/java/cz/forgottenempire/servermanager/serverinstance/ServerLogsServiceTest.java
@@ -1,10 +1,79 @@
 package cz.forgottenempire.servermanager.serverinstance;
 
+import cz.forgottenempire.servermanager.common.PathsFactory;
+import cz.forgottenempire.servermanager.common.ServerType;
+import cz.forgottenempire.servermanager.serverinstance.entities.Arma3Server;
+import cz.forgottenempire.servermanager.serverinstance.entities.Server;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 class ServerLogsServiceTest {
+    private static final int LINES_COUNT = 10;
+    public static final String LOG_NAME = "ARMA3_1.log";
+    public static final long SERVER_ID = 1L;
+
+    @TempDir
+    private Path tempDir;
+
     @Test
-    void name() {
-        new ServerLogsService();
+    void whenGetLastLinesFromServerLog_thenLastLinesReturned() throws IOException {
+        File testLogFile = tempDir.resolve(LOG_NAME).toFile();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(testLogFile));
+        writer.write(getTestLog());
+        writer.close();
+
+        PathsFactory pathsFactory = mock(PathsFactory.class);
+        when(pathsFactory.getServerLogFile(ServerType.ARMA3, SERVER_ID)).thenReturn(testLogFile);
+
+        ServerLogsService serverLogsService = new ServerLogsService(pathsFactory);
+        Server server = new Arma3Server();
+        server.setId(SERVER_ID);
+        server.setType(ServerType.ARMA3);
+        String log = serverLogsService.getLastLinesFromServerLog(server, LINES_COUNT);
+
+        assertThat(log).isNotEmpty();
+        assertThat(log.lines().toList()).hasSize(LINES_COUNT);
+        assertThat(log).isEqualTo(
+                """
+                        Line 4
+                        Line 5
+                        Line 6
+                        Line 7
+                        Line 8
+                        Line 9
+                        Line 10
+                        Line 11
+                        Line 12
+                        Line 13
+                        """
+        );
+    }
+
+    private String getTestLog() {
+        return """
+                Line 1
+                Line 2
+                Line 3
+                Line 4
+                Line 5
+                Line 6
+                Line 7
+                Line 8
+                Line 9
+                Line 10
+                Line 11
+                Line 12
+                Line 13""";
     }
 }
