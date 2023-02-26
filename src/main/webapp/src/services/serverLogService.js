3e753d0f1963023b2cd168cdc50eb2b1e6daafa3
fugasjunior <mail@martinfunda.cz>
2023-02-26 20:24:06 +0100

    Choose default file name if not provided by server

--- src/main/webapp/src/services/serverLogService.js
+++ src/main/webapp/src/services/serverLogService.js
@@ -14,7 +14,15 @@ export function downloadLogFile(id) {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', response.headers['content-disposition'].split('filename=')[1].replace(/"/g, ''));
+
+            let fileName = response.headers['content-disposition']
+                .split('filename=')[1]
+                .replace(/"/g, '');
+            if (!fileName || fileName.length === 0) {
+                fileName = "log.txt";
+            }
+
+            link.setAttribute('download', fileName);
             document.body.appendChild(link);
             link.click();
             link.parentNode.removeChild(link);
