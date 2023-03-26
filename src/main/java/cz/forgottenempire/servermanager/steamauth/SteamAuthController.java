e9b3cad397214c42c4fcc1bb82c9f723987b6fae
fugasjunior <mail@martinfunda.cz>
2023-03-26 17:03:01 +0200

    Feature: clear Steam Auth button (#63)
    * Implement backend for clear auth button
    
    * Adjust logging for clearing steam auth
    
    * implement frontend for clearing steam auth
    
    * add confirmation dialog for clearing credentials
    
    * add success toast after clearing credentials

--- src/main/java/cz/forgottenempire/servermanager/steamauth/SteamAuthController.java
+++ src/main/java/cz/forgottenempire/servermanager/steamauth/SteamAuthController.java
@@ -1,10 +1,10 @@
 package cz.forgottenempire.servermanager.steamauth;
 
 import cz.forgottenempire.servermanager.workshop.SteamAuthDto;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
-@RequestMapping("/api/config")
+@RequestMapping("/api/config/auth")
 class SteamAuthController {
 
     private final SteamAuthService authService;
@@ -23,13 +22,13 @@ class SteamAuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/auth")
+    @PostMapping
     public ResponseEntity<?> setAuthAccount(@RequestBody SteamAuthDto auth) {
         authService.setAuthAccount(auth);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/auth")
+    @GetMapping
     public ResponseEntity<SteamAuthDto> getAuthAccount() {
         SteamAuth steamAuth = authService.getAuthAccount();
         SteamAuthDto dto = new SteamAuthDto();
@@ -37,5 +36,11 @@ class SteamAuthController {
         dto.setSteamGuardToken(steamAuth.getSteamGuardToken());
 
         return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> clearAuthAccount() {
+        authService.clearAuthAccount();
+        return ResponseEntity.noContent().build();
     }
 }
