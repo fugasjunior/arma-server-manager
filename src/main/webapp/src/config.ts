a340449d3ac69d497f10b2140f23e32774d08840
fugasjunior <mail@martinfunda.cz>
2023-06-24 17:05:26 +0200

    Fix changed configuration files

--- src/main/webapp/src/config.ts
+++ src/main/webapp/src/config.ts
@@ -4,8 +4,9 @@ type AppConfig = {
 };
 
 const config: AppConfig = {
-    // apiUrl: "/api",
-    apiUrl: "http://localhost:8080/api",
+    apiUrl: "/api",
+    // apiUrl: "http://localhost:8080/api",
+    // apiUrl: "http://homeserver.lan:8080/api",
     dateFormat: {year: "numeric", month: "2-digit", day: "numeric", hour: "2-digit", minute: "2-digit"},
 };
 
