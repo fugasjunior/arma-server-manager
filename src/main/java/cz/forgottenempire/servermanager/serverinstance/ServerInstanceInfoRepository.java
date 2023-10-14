2bd03c6c3a05322a06a94b6745454bb27bccdba4
fugasjunior <mail@martinfunda.cz>
2023-10-14 21:43:17 +0200

    Remove instanceInfoRepository from ServerProcessService

--- src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerInstanceInfoRepository.java
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerInstanceInfoRepository.java
@@ -3,7 +3,6 @@ package cz.forgottenempire.servermanager.serverinstance;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
@@ -16,26 +15,7 @@ class ServerInstanceInfoRepository {
         return new ArrayList<>(instanceInfoMap.values());
     }
 
-    public ServerInstanceInfo getServerInstanceInfo(Long id) {
-        return Optional.ofNullable(instanceInfoMap.get(id)).orElseGet(() -> {
-            ServerInstanceInfo instanceInfo = createEmptyInstanceInfo(id);
-            instanceInfoMap.put(id, instanceInfo);
-            return instanceInfo;
-        });
-    }
-
     public void storeServerInstanceInfo(Long id, ServerInstanceInfo instanceInfo) {
         instanceInfoMap.put(id, instanceInfo);
-    }
-
-    private ServerInstanceInfo createEmptyInstanceInfo(Long id) {
-        return ServerInstanceInfo.builder()
-                .id(id)
-                .startedAt(null)
-                .playersOnline(0)
-                .process(null)
-                .version("")
-                .map("")
-                .build();
     }
 }
