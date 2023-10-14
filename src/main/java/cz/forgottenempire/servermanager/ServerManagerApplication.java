d9e90a9a19640ed1e6585113ca527bb7ad796693
fugasjunior <mail@martinfunda.cz>
2023-10-14 18:55:07 +0200

    Add @EnableSpringConfigured annotation

--- src/main/java/cz/forgottenempire/servermanager/ServerManagerApplication.java
+++ src/main/java/cz/forgottenempire/servermanager/ServerManagerApplication.java
@@ -3,11 +3,13 @@ package cz.forgottenempire.servermanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableSpringConfigured
 public class ServerManagerApplication {
 
     public static void main(String[] args) {
