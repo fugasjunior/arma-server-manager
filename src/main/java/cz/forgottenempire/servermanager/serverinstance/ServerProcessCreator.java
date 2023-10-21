b0a94ebfb80267a01d44e6f259a07670aa9649a3
fugasjunior <mail@martinfunda.cz>
2023-10-21 18:23:24 +0200

    Extract ServerProcessCreator

---
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/ServerProcessCreator.java
@@ -0,0 +1,25 @@
+package cz.forgottenempire.servermanager.serverinstance;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ServerProcessCreator {
+    public Process startProcessWithRedirectedOutput(File executable, List<String> parameters, File outputFile)
+            throws IOException {
+        File directory = executable.getParentFile();
+        List<String> commands = new ArrayList<String>();
+        commands.add(executable.getAbsolutePath());
+        commands.addAll(parameters);
+
+        return new ProcessBuilder(commands)
+                .directory(directory)
+                .redirectErrorStream(true)
+                .redirectOutput(ProcessBuilder.Redirect.appendTo(outputFile))
+                .start();
+    }
+}
