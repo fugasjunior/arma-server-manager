c81b586c6205a74b843fffb37886da19a5b6fa47
fugasjunior <mail@martinfunda.cz>
2023-11-30 21:21:28 +0100

    Create AutomaticRestartTask

---
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/AutomaticRestartTask.java
@@ -0,0 +1,45 @@
+package cz.forgottenempire.servermanager.serverinstance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.TaskScheduler;
+
+import java.time.*;
+import java.util.concurrent.ScheduledFuture;
+
+@Configurable
+public class AutomaticRestartTask {
+    private final ServerProcess serverProcess;
+    private final LocalTime restartTime;
+    private TaskScheduler taskScheduler;
+    private ScheduledFuture<?> job;
+
+    public AutomaticRestartTask(ServerProcess serverProcess, LocalTime restartTime) {
+        this.serverProcess = serverProcess;
+        this.restartTime = restartTime;
+    }
+
+    public AutomaticRestartTask schedule() {
+        Instant restartInstant = getNearestFutureInstantOf(restartTime);
+        job = taskScheduler.scheduleAtFixedRate(serverProcess::restart, restartInstant, Duration.ofDays(1));
+        return this;
+    }
+
+    public void cancel() {
+        job.cancel(false);
+    }
+
+    private static Instant getNearestFutureInstantOf(LocalTime localTime) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime upcomingDateTime = LocalDateTime.of(currentDateTime.toLocalDate(), localTime);
+        if (currentDateTime.isAfter(upcomingDateTime)) {
+            upcomingDateTime = upcomingDateTime.plusDays(1);
+        }
+        return upcomingDateTime.atZone(ZoneId.systemDefault()).toInstant();
+    }
+
+    @Autowired
+    void setTaskScheduler(TaskScheduler taskScheduler) {
+        this.taskScheduler = taskScheduler;
+    }
+}
