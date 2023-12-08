4d68d449665ee65debb81c0b6834644a4071df23
fugasjunior <mail@martinfunda.cz>
2023-12-08 20:05:56 +0100

    Change format of server entry header

--- src/main/webapp/src/components/servers/ServerHeader.tsx
+++ src/main/webapp/src/components/servers/ServerHeader.tsx
@@ -1,19 +1,14 @@
-import {Arma3ServerDto, ServerDto} from "../../dtos/ServerDto.ts";
-import {Stack} from "@mui/material";
+import {ServerDto} from "../../dtos/ServerDto.ts";
+import {Stack, Typography} from "@mui/material";
 import {ServerIcon} from "./ServerIcon.tsx";
 
 export function ServerHeader(props: { server: ServerDto }) {
-    return <Stack direction="row" spacing={2}>
+    return <Stack direction="row" spacing={2} alignItems="center">
         <ServerIcon serverType={props.server.type}/>
-
         <Stack>
-        <p>{props.server.name}</p>
-        <p>Port: {props.server.port}</p>
-        {props.server.description && <p>{props.server.description}</p>}
-        {props.server.type === "ARMA3" && <>
-            {(props.server as Arma3ServerDto).activeMods.length > 0 && <p>Mods active</p>}
-            {(props.server as Arma3ServerDto).activeDLCs.length > 0 && <p>Creator DLC(s) active</p>}
-        </>}
+            <Typography variant="h6">{props.server.name}</Typography>
+            {props.server.description && <Typography variant="body2">{props.server.description}</Typography>}
+            <Typography variant="body2">Port: {props.server.port}</Typography>
         </Stack>
     </Stack>;
 }
