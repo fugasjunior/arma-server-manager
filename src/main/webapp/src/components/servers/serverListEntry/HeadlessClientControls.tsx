61fe1892bdfacc67b1a3c7fc461bd9e4397987b8
fugasjunior <mail@martinfunda.cz>
2023-12-10 14:46:01 +0100

    Move HeadlessClientControls to separate file

---
+++ src/main/webapp/src/components/servers/serverListEntry/HeadlessClientControls.tsx
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import {addHeadlessClient, removeHeadlessClient} from "../../../services/serversService.ts";
+import {toast} from "material-react-toastify";
+import {Avatar, Stack, Typography} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from "@mui/icons-material/Remove";
+import {blue, grey} from "@mui/material/colors";
+import AddIcon from "@mui/icons-material/Add";
+
+export function HeadlessClientControls(props: { serverStatus: ServerInstanceInfoDto, serverId: number }) {
+    const [headlessClientsCount, setHeadlessClientsCount] = useState(props.serverStatus.headlessClientsCount);
+
+    const handleAddHeadlessClient = async () => {
+        setHeadlessClientsCount(prevState => ++prevState);
+        await addHeadlessClient(props.serverId);
+        toast.success("Headless client started.");
+    }
+
+    const handleRemoveHeadlessClient = async () => {
+        setHeadlessClientsCount(prevState => --prevState);
+        await removeHeadlessClient(props.serverId);
+        toast.success("Headless client stopped.");
+    }
+
+    return <Stack spacing={1}>
+        <Typography>Headless clients</Typography>
+        <Stack direction="row">
+            <IconButton color="primary" aria-label="remove headless client"
+                        disabled={headlessClientsCount < 1}
+                        onClick={handleRemoveHeadlessClient}
+            >
+                <RemoveIcon/>
+            </IconButton>
+            <Avatar sx={headlessClientsCount > 0 ? {bgcolor: blue[500]} : {bgcolor: grey[400]}}>
+                {headlessClientsCount}
+            </Avatar>
+            <IconButton color="primary" aria-label="add headless client"
+                        onClick={handleAddHeadlessClient}
+            >
+                <AddIcon/>
+            </IconButton>
+        </Stack>
+    </Stack>;
+}
--- src/main/webapp/src/components/servers/serverListEntry/ServerListEntryDetails.tsx
+++ src/main/webapp/src/components/servers/serverListEntry/ServerListEntryDetails.tsx
@@ -1,52 +1,10 @@
 import {ServerDto} from "../../../dtos/ServerDto.ts";
-import {Avatar, Button, Stack, Typography} from "@mui/material";
+import {Button, Stack} from "@mui/material";
 import ModEditButton from "../ModEditButton.tsx";
 import ListBuilderDLCsEdit from "../ListBuilderDLCsEdit.tsx";
 import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import AutomaticRestartSettings from "./AutomaticRestartSettings.tsx";
-import IconButton from "@mui/material/IconButton";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import {useState} from "react";
-import {blue, grey} from '@mui/material/colors';
-import {addHeadlessClient, removeHeadlessClient} from "../../../services/serversService.ts";
-import {toast} from "material-react-toastify";
-
-function HeadlessClientControls(props: { serverStatus: ServerInstanceInfoDto, serverId: number }) {
-    const [headlessClientsCount, setHeadlessClientsCount] = useState(props.serverStatus.headlessClientsCount);
-
-    const handleAddHeadlessClient = async () => {
-        setHeadlessClientsCount(prevState => ++prevState);
-        await addHeadlessClient(props.serverId);
-        toast.success("Headless client started.");
-    }
-
-    const handleRemoveHeadlessClient = async () => {
-        setHeadlessClientsCount(prevState => --prevState);
-        await removeHeadlessClient(props.serverId);
-        toast.success("Headless client stopped.");
-    }
-
-    return <Stack spacing={1}>
-        <Typography>Headless clients</Typography>
-        <Stack direction="row">
-            <IconButton color="primary" aria-label="remove headless client"
-                        disabled={headlessClientsCount < 1}
-                        onClick={handleRemoveHeadlessClient}
-            >
-                <RemoveIcon/>
-            </IconButton>
-            <Avatar sx={headlessClientsCount > 0 ? {bgcolor: blue[500]} : {bgcolor: grey[400]}}>
-                {headlessClientsCount}
-            </Avatar>
-            <IconButton color="primary" aria-label="add headless client"
-                        onClick={handleAddHeadlessClient}
-            >
-                <AddIcon/>
-            </IconButton>
-        </Stack>
-    </Stack>;
-}
+import {HeadlessClientControls} from "./HeadlessClientControls.tsx";
 
 export function ServerListEntryDetails(props: {
     server: ServerDto,
