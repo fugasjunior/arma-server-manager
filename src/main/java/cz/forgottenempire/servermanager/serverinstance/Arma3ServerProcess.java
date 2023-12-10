a623cdb3ba1be8d1215d319a8f01f8dd15a21bb2
fugasjunior <mail@martinfunda.cz>
2023-12-10 10:27:26 +0100

    Create Arma 3 specific ServerProcess

---
+++ src/main/java/cz/forgottenempire/servermanager/serverinstance/Arma3ServerProcess.java
@@ -0,0 +1,62 @@
+package cz.forgottenempire.servermanager.serverinstance;
+
+import cz.forgottenempire.servermanager.serverinstance.entities.Arma3Server;
+import cz.forgottenempire.servermanager.serverinstance.entities.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+@Configurable
+public class Arma3ServerProcess extends ServerProcess {
+    private final long serverId;
+    private final Deque<HeadlessClient> headlessClients;
+
+    private ServerInstanceService serverInstanceService;
+
+    public Arma3ServerProcess(long serverId) {
+        super(serverId);
+        headlessClients = new LinkedList<>();
+        this.serverId = serverId;
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        for (int i = 0; i < headlessClients.size(); i++) {
+            removeHeadlessClient();
+        }
+    }
+
+    @Override
+    public void restart() {
+        int countOfHeadlessClients = headlessClients.size();
+        super.restart();
+        for (int i = 0; i < countOfHeadlessClients; i++) {
+            addHeadlessClient();
+        }
+    }
+
+    public void addHeadlessClient() {
+        Server server = serverInstanceService.getServer(serverId).orElseThrow();
+        if (!(server instanceof Arma3Server arma3Server)) {
+            throw new IllegalStateException("Server ID " + server + " is not Arma 3 server");
+        }
+        headlessClients.push(new HeadlessClient(headlessClients.size() + 1, arma3Server).start());
+        instanceInfo.setHeadlessClientsCount(headlessClients.size());
+    }
+
+    public void removeHeadlessClient() {
+        if (headlessClients.isEmpty()) {
+            return;
+        }
+        headlessClients.pop().stop();
+        instanceInfo.setHeadlessClientsCount(headlessClients.size());
+    }
+
+    @Autowired
+    void setServerInstanceService(ServerInstanceService serverInstanceService) {
+        this.serverInstanceService = serverInstanceService;
+    }
+}
