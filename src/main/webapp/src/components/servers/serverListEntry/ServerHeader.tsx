99b7127ac46e3c0ec27740aaf483db8bd483e3ac
fugasjunior <mail@martinfunda.cz>
2023-12-14 20:30:33 +0100

    Add server type to server header

--- src/main/webapp/src/components/servers/serverListEntry/ServerHeader.tsx
+++ src/main/webapp/src/components/servers/serverListEntry/ServerHeader.tsx
@@ -1,12 +1,16 @@
-import {ServerDto} from "../../../dtos/ServerDto.ts";
+import {ServerDto, ServerType} from "../../../dtos/ServerDto.ts";
 import {Stack, Typography} from "@mui/material";
 import {ServerIcon} from "./ServerIcon.tsx";
+import SERVER_NAMES from "../../../util/serverNames.ts";
 
 export function ServerHeader(props: { server: ServerDto }) {
     return <Stack direction="row" spacing={2} alignItems="center">
         <ServerIcon serverType={props.server.type}/>
         <Stack>
             <Typography variant="h6">{props.server.name}</Typography>
+            <Typography variant="subtitle2">
+                {SERVER_NAMES.get(ServerType[props.server.type as keyof typeof ServerType])}
+            </Typography>
             {props.server.description && <Typography variant="body2">{props.server.description}</Typography>}
             <Typography variant="body2">Port: {props.server.port}</Typography>
         </Stack>
