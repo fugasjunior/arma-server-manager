cf48302bdc20344c6a140f9dd88e2e17ec5c4e0d
fugasjunior <mail@martinfunda.cz>
2024-04-13 15:41:32 +0200

    Add support for Reaction Forces CDLC (#110)

--- src/main/java/cz/forgottenempire/servermanager/workshop/Arma3CDLC.java
+++ src/main/java/cz/forgottenempire/servermanager/workshop/Arma3CDLC.java
@@ -7,6 +7,7 @@ import java.util.List;
 public enum Arma3CDLC {
     CSLA_IRON_CURTAIN("CSLA Iron Curtain", "csla"),
     GLOBAL_MOBILIZATION("Global Mobilization", "gm"),
+    REACTION_FORCES("Reaction Forces", "rf"),
     SOG_PRAIRIE_FIRE("S.O.G. Prairie Fire", "vn"),
     SPEARHEAD_1944("Spearhead 1944", "spe"),
     WESTERN_SAHARA("Western Sahara", "ws");
@@ -28,13 +29,14 @@ public enum Arma3CDLC {
     }
 
     public static List<Arma3CDLC> getAll() {
-        return List.of(CSLA_IRON_CURTAIN, GLOBAL_MOBILIZATION, SOG_PRAIRIE_FIRE, SPEARHEAD_1944, WESTERN_SAHARA);
+        return List.of(CSLA_IRON_CURTAIN, GLOBAL_MOBILIZATION, REACTION_FORCES, SOG_PRAIRIE_FIRE, SPEARHEAD_1944, WESTERN_SAHARA);
     }
 
     public static Arma3CDLC fromId(String id) {
         return switch (id) {
             case "csla" -> CSLA_IRON_CURTAIN;
             case "gm" -> GLOBAL_MOBILIZATION;
+            case "rf" -> REACTION_FORCES;
             case "vn" -> SOG_PRAIRIE_FIRE;
             case "spe" -> SPEARHEAD_1944;
             case "ws" -> WESTERN_SAHARA;
