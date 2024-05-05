1c578652b45bcbf2a1daec99950165afed6a3cce
fugasjunior <mail@martinfunda.cz>
2024-05-05 15:45:14 +0200

    Fix migrate file

--- backend/src/main/resources/db/migration/V1_3_1__SERVER_INSTALLATION_BRANCH.sql
+++ backend/src/main/resources/db/migration/V1_3_1__SERVER_INSTALLATION_BRANCH.sql
@@ -1,3 +1,18 @@
+ALTER TABLE server_installation
+    ADD branch VARCHAR(255) NOT NULL DEFAULT 'PUBLIC';
+
+-- Update existing server installations
+UPDATE server_installation
+SET branch = IF(type = 'ARMA3', 'CREATORDLC', 'PUBLIC');
+
+-- Create default server installation instances if they don't exist yet
+INSERT IGNORE INTO server_installation (type, branch)
+VALUES ('ARMA3', 'CREATORDLC'),
+       ('DAYZ', 'PUBLIC'),
+       ('DAYZ_EXP', 'PUBLIC'),
+       ('REFORGER', 'PUBLIC');
+
+-- Create table
 CREATE TABLE available_branches
 (
     type   VARCHAR(255) NOT NULL,
@@ -11,20 +26,6 @@ VALUES ('ARMA3', 'CREATORDLC'),
        ('ARMA3', 'PUBLIC'),
        ('ARMA3', 'PROFILING'),
        ('ARMA3', 'CONTACT'),
-       ('DAYZ', 'PUBLIC'),
-       ('DAYZ_EXP', 'PUBLIC'),
-       ('REFORGER', 'PUBLIC');
-
-ALTER TABLE server_installation
-    ADD branch VARCHAR(255) NOT NULL DEFAULT 'PUBLIC';
-
--- Update existing server installations
-UPDATE server_installation
-SET branch = IF(type = 'ARMA3', 'CREATORDLC', 'PUBLIC');
-
--- Create default server installation instances if they don't exist yet
-INSERT IGNORE INTO server_installation (type, branch)
-VALUES ('ARMA3', 'CREATORDLC'),
        ('DAYZ', 'PUBLIC'),
        ('DAYZ_EXP', 'PUBLIC'),
        ('REFORGER', 'PUBLIC');
