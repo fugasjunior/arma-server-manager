b7e4b217b32879397143c8a602b6f590e0b94554
fugasjunior <mail@martinfunda.cz>
2024-05-07 20:05:07 +0200

    Create test whenHeadlessClientIsStartedWithPasswordProtectedServer_thenProcessCreatedWithPasswordParameter

--- backend/src/test/java/cz/forgottenempire/servermanager/serverinstance/headlessclient/HeadlessClientTest.java
+++ backend/src/test/java/cz/forgottenempire/servermanager/serverinstance/headlessclient/HeadlessClientTest.java
@@ -17,6 +17,7 @@ class HeadlessClientTest {
     public static final long SERVER_ID = 1L;
     public static final int HEADLESS_CLIENT_ID = 10;
     public static final int SERVER_PORT = 2302;
+    public static final String SERVER_PASSWORD = "hunter2";
 
     @Test
     void whenHeadlessClientIsStarted_thenProcessCreatedWithCorrectParameters() throws IOException {
@@ -40,6 +41,31 @@ class HeadlessClientTest {
         headlessClient.start();
 
         List<String> expectedParameters = List.of("-client", "-connect=127.0.0.1:2302");
+        verify(serverProcessCreator).startProcessWithRedirectedOutput(serverExecutable, expectedParameters, logFile);
+    }
+    @Test
+    void whenHeadlessClientIsStartedWithPasswordProtectedServer_thenProcessCreatedWithPasswordParameter() throws IOException {
+        Arma3Server server = mock(Arma3Server.class, withSettings().stubOnly());
+        when(server.getId()).thenReturn(SERVER_ID);
+        when(server.getPort()).thenReturn(SERVER_PORT);
+        when(server.getPassword()).thenReturn(SERVER_PASSWORD);
+
+        HeadlessClient headlessClient = new HeadlessClient(HEADLESS_CLIENT_ID, server);
+        PathsFactory pathsFactory = mock(PathsFactory.class);
+
+        File logFile = mock(File.class, withSettings().stubOnly());
+        when(pathsFactory.getHeadlessClientLogFile(SERVER_ID, HEADLESS_CLIENT_ID)).thenReturn(logFile);
+
+        File serverExecutable = mock(File.class, withSettings().stubOnly());
+        when(pathsFactory.getServerExecutableWithFallback(ServerType.ARMA3)).thenReturn(serverExecutable);
+
+        headlessClient.setPathsFactory(pathsFactory);
+        ServerProcessCreator serverProcessCreator = mock(ServerProcessCreator.class);
+        headlessClient.setServerProcessCreator(serverProcessCreator);
+
+        headlessClient.start();
+
+        List<String> expectedParameters = List.of("-client", "-connect=127.0.0.1:2302", "-password=" + SERVER_PASSWORD);
         verify(serverProcessCreator).startProcessWithRedirectedOutput(serverExecutable, expectedParameters, logFile);
     }
 }
