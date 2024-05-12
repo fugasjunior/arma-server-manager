584d3369c4194591e7e92a9807236c806bccb574
fugasjunior <mail@martinfunda.cz>
2024-05-12 13:36:30 +0200

    Create controller

---
+++ backend/src/main/java/cz/forgottenempire/servermanager/steamcmd/SteamCmdItemInfoController.java
@@ -0,0 +1,28 @@
+package cz.forgottenempire.servermanager.steamcmd;
+
+import cz.forgottenempire.servermanager.steamcmd.outputprocessor.SteamCmdItemInfo;
+import cz.forgottenempire.servermanager.steamcmd.outputprocessor.SteamCmdItemInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/steamcmd")
+class SteamCmdItemInfoController {
+
+    private final SteamCmdItemInfoRepository itemInfoRepository;
+
+    @Autowired
+    public SteamCmdItemInfoController(SteamCmdItemInfoRepository itemInfoRepository) {
+        this.itemInfoRepository = itemInfoRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<Long, SteamCmdItemInfo>> getItemInfos() {
+        return ResponseEntity.ok(itemInfoRepository.getAll());
+    }
+}
