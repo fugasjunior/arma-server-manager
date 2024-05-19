8393105b7975e7a9241e91a5e513da1c19a02a53
fugasjunior <mail@martinfunda.cz>
2024-05-19 17:37:39 +0200

    Move duplicate button

--- frontend/src/components/servers/serverListEntry/ServerListEntryDetails.tsx
+++ frontend/src/components/servers/serverListEntry/ServerListEntryDetails.tsx
@@ -5,6 +5,7 @@ import ListBuilderDLCsEdit from "../ListBuilderDLCsEdit.tsx";
 import TextSnippetIcon from "@mui/icons-material/TextSnippet";
 import AutomaticRestartSettings from "./AutomaticRestartSettings.tsx";
 import {HeadlessClientControls} from "./HeadlessClientControls.tsx";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 export function ServerListEntryDetails(props: {
     server: ServerDto,
@@ -12,27 +13,31 @@ export function ServerListEntryDetails(props: {
     onClick: () => void,
     onDuplicateServer: (server: ServerDto) => void
 }) {
-    return <Stack direction="row" spacing={2} justifyItems="center" alignItems="center">
-        <ModEditButton server={props.server} serverStatus={props.serverStatus}/>
-        {props.server.type === "ARMA3" &&
-            <ListBuilderDLCsEdit status={props.serverStatus} server={props.server}/>}
-        <Button
-            variant="contained"
-            startIcon={<TextSnippetIcon/>} onClick={props.onClick}
-            color="info">
-            Logs
-        </Button>
+    return <Stack direction="row">
+        <Stack direction="row" spacing={2} justifyItems="center" alignItems="center" sx={{flexGrow: 1}}
+               justifyContent="flex-start">
+            <ModEditButton server={props.server} serverStatus={props.serverStatus}/>
+            {props.server.type === "ARMA3" &&
+                <ListBuilderDLCsEdit status={props.serverStatus} server={props.server}/>}
+            <Button
+                variant="contained"
+                startIcon={<TextSnippetIcon/>} onClick={props.onClick}
+                color="info">
+                Logs
+            </Button>
 
-        <Button
-            variant="contained"
-            startIcon={<TextSnippetIcon/>} onClick={() => props.onDuplicateServer(props.server)}
-            color="info">
-            Duplicate
-        </Button>
+            <AutomaticRestartSettings serverId={props.server.id!} dto={props.server.automaticRestart}/>
 
-        <AutomaticRestartSettings serverId={props.server.id!} dto={props.server.automaticRestart}/>
-
-        {props.serverStatus?.alive && props.server.type === "ARMA3" &&
-            <HeadlessClientControls serverId={props.server.id!} serverStatus={props.serverStatus}/>}
+            {props.serverStatus?.alive && props.server.type === "ARMA3" &&
+                <HeadlessClientControls serverId={props.server.id!} serverStatus={props.serverStatus}/>}
+        </Stack>
+        <Stack direction="row" sx={{flexGrow: 0}}>
+            <Button
+                variant="outlined"
+                startIcon={<ContentCopyIcon/>} onClick={() => props.onDuplicateServer(props.server)}
+                color="info">
+                Duplicate
+            </Button>
+        </Stack>
     </Stack>;
 }
